2bceb3650be82833220455d9562cd89c481c4716
RanNewUser <149963228+RanNewUser@users.noreply.github.com>
2023-11-05 18:48:21 +0100

    Add files via upload

---
+++ Code/script.js
@@ -0,0 +1,36 @@
+const track = document.getElementById("image-track");
+
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+const handleOnMove = e => {
+  if(track.dataset.mouseDownAt === "0") return;
+  
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+  
+  const percentage = (mouseDelta / maxDelta) * -95,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, -28), -75);
+  
+  track.dataset.percentage = nextPercentage;
+  
+  track.style.transform = `translate(${nextPercentage}%, -50%)`;
+
+  for (const image of track.getElementsByClassName("image")) {
+    image.style.objectPosition = `${100 + nextPercentage}% center`;
+  }
+
+  if (now < duration) {
+    animationFrameId = requestAnimationFrame(animateImages);
+  }
+};
+      // Attach event listeners
+      track.addEventListener("mousedown", handleOnDown);
+      track.addEventListener("mouseup", handleOnUp);
+      track.addEventListener("mousemove", handleOnMove);
+
